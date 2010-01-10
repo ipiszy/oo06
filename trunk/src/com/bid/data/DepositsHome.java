20c27b8cf0129a1137455f9f5a4f3a50e88ddebd
fenggang.wu <fenggang.wu@eff8e6bc-f561-11de-aa3c-59de0406b4f5>
2010-01-10 10:52:40 +0000

    hibernate files and its generated ones

---
+++ trunk/src/com/bid/data/DepositsHome.java
@@ -0,0 +1,124 @@
+package com.bid.data;
+
+// Generated 2010-1-10 18:44:41 by Hibernate Tools 3.2.4.GA
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import static org.hibernate.criterion.Example.create;
+
+/**
+ * Home object for domain model class Deposits.
+ * @see com.bid.data.Deposits
+ * @author Hibernate Tools
+ */
+public class DepositsHome {
+
+	private static final Log log = LogFactory.getLog(DepositsHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Deposits transientInstance) {
+		log.debug("persisting Deposits instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Deposits instance) {
+		log.debug("attaching dirty Deposits instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Deposits instance) {
+		log.debug("attaching clean Deposits instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Deposits persistentInstance) {
+		log.debug("deleting Deposits instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Deposits merge(Deposits detachedInstance) {
+		log.debug("merging Deposits instance");
+		try {
+			Deposits result = (Deposits) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Deposits findById(com.bid.data.DepositsId id) {
+		log.debug("getting Deposits instance with id: " + id);
+		try {
+			Deposits instance = (Deposits) sessionFactory.getCurrentSession()
+					.get("com.bid.data.Deposits", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List<Deposits> findByExample(Deposits instance) {
+		log.debug("finding Deposits instance by example");
+		try {
+			List<Deposits> results = (List<Deposits>) sessionFactory
+					.getCurrentSession()
+					.createCriteria("com.bid.data.Deposits").add(
+							create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
