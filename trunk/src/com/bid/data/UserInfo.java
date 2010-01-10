40a0008b927643f1f5ae72144ee45cf0ef63b0e9
fenggang.wu <fenggang.wu@eff8e6bc-f561-11de-aa3c-59de0406b4f5>
2010-01-10 12:51:13 +0000

    1. Partial Implementation of UserManager 2. Some package deployment changes to be compatible with gwt

--- trunk/src/com/bid/data/UserInfo.java
+++ trunk/src/com/bid/data/UserInfo.java
@@ -2,10 +2,32 @@ package com.bid.data;
 
 public class UserInfo {
 
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+	public String userName;
+	public double userBalance = 0;
+	public String userPass;
+	public String userBankAccount;
+	public String userMailBox;
+
+	UserInfo(String name, int balance, String pswd, String bankAccount, String mailBox)
+	{
+		this.userName = name;
+		this.userBalance = balance;
+		this.userPass = pswd;
+		this.userBankAccount = bankAccount;
+		this.userMailBox = mailBox;
 	}
+
+//	boolean setuserBalance(String userName,int charge);
+//
+//	boolean setuserPass(String userName,String newPass);
+//
+//	boolean setuserBankAccount(String userName,String newBankAccount);
+//
+//	boolean setuserMailBox(String userName,String newMailBox)
+//	public String getName() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 }
 
--- trunk/src/com/bid/dataMgr/UserMgr.java
+++ trunk/src/com/bid/dataMgr/UserMgr.java
@@ -1,31 +1,107 @@
 package com.bid.dataMgr;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.bid.data.UserInfo;
+import com.bid.data.Users;
 
 public class UserMgr {
-	public boolean isValid(String userName, String psw){
+	private static Log log = LogFactory.getLog(UserMgr.class);
+
+	public boolean isValid(String userName, String psw) {
+		Session s = HibernateUtility.currentSession();
+		boolean returnValue = false;
+		try {
+			HibernateUtility.beginTransaction();
+			Users user = (Users) s.get(Users.class, userName);
+			HibernateUtility.commitTransaction();
+
+			if (user == null)
+				returnValue = false;
+			else if (!user.getUserPass().equals(psw))
+				returnValue = false;
+			else
+				returnValue = true;
+			// else if (user.isDisabled() == true)
+			// returnString = "disabled";
+			// else
+			// returnString = user.getType();
+
+		} catch (HibernateException e) {
+			HibernateUtility.commitTransaction();
+			log.fatal(e);
+			returnValue = false;
+		}
+		HibernateUtility.closeSession();
+		return returnValue;
+	}
+
+	public boolean addUser(UserInfo userInfo) {
+		boolean flag = true;
+		Session s = HibernateUtility.currentSession();
+		try {
+			Users user = new Users();
+			user.setUserName(userInfo.userName);
+			user.setUserPass(userInfo.userPass);
+			user.setUserBalance(userInfo.userBalance);
+			user.setUserBankAccount(userInfo.userBankAccount);
+			user.setUserMailBox(userInfo.userMailBox);
+
+			HibernateUtility.beginTransaction();
+			s.saveOrUpdate(user);
+			HibernateUtility.commitTransaction();
+		} catch (HibernateException e) {
+			HibernateUtility.commitTransaction();
+			e.printStackTrace();
+			log.fatal(e);
+			flag = false;
+		}
+
+		HibernateUtility.closeSession();
+		return flag;
+
+	}
+
+	public boolean charge(String userName, double money) {
+		// TODO
 		return false;
 	}
 
-	public boolean addUser(UserInfo userInfo){
+	public boolean transfer(double money/* (+,-) */) {
+		// TODO
 		return false;
 	}
 
-	public boolean charge(String userName, double money){
-		return false;
-	}
-
-	public boolean transfer(double money/*(+,-)*/) {
-		return false;
-	}
-
+	// is this necessary?
+	// Wu Fenggang
 	public void login(String userName) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	public boolean hasUser(String userName){
-		return false;
+
+	public boolean hasUser(String userName) {
+		Session s = HibernateUtility.currentSession();
+		boolean returnValue = false;
+		try {
+			HibernateUtility.beginTransaction();
+			Users user = (Users) s.get(Users.class, userName);
+			HibernateUtility.commitTransaction();
+
+			if (user == null)
+				returnValue = false;
+			returnValue = true;
+
+		} catch (HibernateException e) {
+			HibernateUtility.commitTransaction();
+			log.fatal(e);
+			returnValue = false;
+		}
+		HibernateUtility.closeSession();
+		return returnValue;
 	}
 
 }
