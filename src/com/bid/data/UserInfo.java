f7d72b5d6bf7a923b68403200418732bdb565ec2
fenggang.wu <fenggang.wu@eff8e6bc-f561-11de-aa3c-59de0406b4f5>
2010-01-10 15:36:40 +0000

    new version of the messages

--- src/com/bid/data/UserInfo.java
+++ src/com/bid/data/UserInfo.java
@@ -8,33 +8,63 @@ package com.bid.data;
  */
 public class UserInfo {
 
-	public String userName;
-	public double userBalance = 0;
-	public String userPass;
-	public String userBankAccount;
-	public String userMailBox;
+	private String userName;
+	private double userBalance = 0;
+	private String userPass;
+	private String userBankAccount;
+	private String userMailBox;
 
-	UserInfo(String name, int balance, String pswd, String bankAccount, String mailBox)
+	public UserInfo(){
+		
+	}
+	public UserInfo(String name, int balance, String pswd, String bankAccount, String mailBox)
 	{
-		this.userName = name;
-		this.userBalance = balance;
-		this.userPass = pswd;
-		this.userBankAccount = bankAccount;
-		this.userMailBox = mailBox;
+		this.setUserName(name);
+		this.setUserBalance(balance);
+		this.setUserPass(pswd);
+		this.setUserBankAccount(bankAccount);
+		this.setUserMailBox(mailBox);
 	}
 
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-//	boolean setuserBalance(String userName,int charge);
-//
-//	boolean setuserPass(String userName,String newPass);
-//
-//	boolean setuserBankAccount(String userName,String newBankAccount);
-//
-//	boolean setuserMailBox(String userName,String newMailBox)
-//	public String getName() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserBalance(double userBalance) {
+		this.userBalance = userBalance;
+	}
+
+	public double getUserBalance() {
+		return userBalance;
+	}
+
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+
+	public String getUserPass() {
+		return userPass;
+	}
+
+	public void setUserBankAccount(String userBankAccount) {
+		this.userBankAccount = userBankAccount;
+	}
+
+	public String getUserBankAccount() {
+		return userBankAccount;
+	}
+
+	public void setUserMailBox(String userMailBox) {
+		this.userMailBox = userMailBox;
+	}
+
+	public String getUserMailBox() {
+		return userMailBox;
+	}
 
 }
 
