7cfd7942d8b3019af99d57db97b21c11d936ecd3
fenggang.wu <fenggang.wu@eff8e6bc-f561-11de-aa3c-59de0406b4f5>
2010-01-11 15:11:55 +0000

    Update the testing class

--- src/com/bid/dataMgr/TestMain.java
+++ src/com/bid/dataMgr/TestMain.java
@@ -1,5 +1,11 @@
 package com.bid.dataMgr;
 
+import java.util.Iterator;
+import java.util.List;
+
+import com.bid.exchange.Item;
+import com.bid.exchange.ItemDigest;
+
 public class TestMain {
 	public static void main(String[]arg){
 		DataBoundary b = new DataBoundary();
@@ -14,6 +20,49 @@ public class TestMain {
 		
 		// browse test
 		
+		List<ItemDigest> itemlist;
+		Item sampleItem;
+		
+		System.out.println("testing getDyingItems(long , long ):");
+		itemlist = b.getDyingItems(0, 250);
+		Iterator<ItemDigest> iter = itemlist.iterator();
+		while (iter.hasNext()){
+			ItemDigest it = iter.next();
+			it.dump();
+		}
+		
+		System.out.println("testing getDyingItems(long , long , long ):");
+		itemlist = b.getDyingItems(0, 250, 1001);
+		iter = itemlist.iterator();
+		while (iter.hasNext()){
+			ItemDigest it = iter.next();
+			it.dump();
+		}
+		
+		System.out.println("testing getLatestItems(long , long ):");
+		itemlist = b.getLatestItems(0, 250);
+		iter = itemlist.iterator();
+		while (iter.hasNext()){
+			ItemDigest it = iter.next();
+			it.dump();
+		}
+		
+		System.out.println("testing getLatestItems(long , long, long ):");
+		itemlist = b.getLatestItems(0, 250, 1001);
+		iter = itemlist.iterator();
+		while (iter.hasNext()){
+			ItemDigest it = iter.next();
+			it.dump();
+		}
+		
+		System.out.println("testing getItem(long ):");
+		sampleItem = b.getItem(90001);
+		sampleItem.dump();
+		
+		System.out.println("testing getItem(long ):");
+		sampleItem  = b.getItemInfo(90001);
+		sampleItem.dump();
+
 		
 		// bid test
 		
