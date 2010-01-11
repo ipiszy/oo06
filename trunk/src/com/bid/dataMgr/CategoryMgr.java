67f07c435ddd6d8c7f69d78a47fd987526a9c7ac
fenggang.wu <fenggang.wu@eff8e6bc-f561-11de-aa3c-59de0406b4f5>
2010-01-11 12:51:40 +0000

    Completing the Category Manager

--- trunk/src/com/bid/dataMgr/CategoryMgr.java
+++ trunk/src/com/bid/dataMgr/CategoryMgr.java
@@ -1,18 +1,46 @@
 package com.bid.dataMgr;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
 import com.bid.data.Category;
-import com.bid.data.Sorts;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class CategoryMgr {
 
+	private static Log log = LogFactory.getLog(CategoryMgr.class);
+
 	public List<Category> getAllCategories() {
-		List<Category> cList = new LinkedList<Category>();
-		Sorts sort = new Sorts();
-		Category category = new Category();
-		return cList;
+		Session s = HibernateUtility.currentSession();
+		ArrayList<Category> categoryList = new ArrayList<Category>();
+		try {
+			HibernateUtility.beginTransaction();
+
+			List sortList = s.createSQLQuery(
+					"SELECT sortID, sortName from sorts").list();
+			HibernateUtility.commitTransaction();
+
+			for (Object obj : sortList) {
+				Object[] o = (Object[]) obj;
+				long sortID = Long.parseLong(o[0].toString());
+				String sortName = o[1].toString();
+				categoryList.add(new Category(sortID, sortName));
+			}
+
+		} catch (HibernateException e) {
+			HibernateUtility.commitTransaction();
+			e.printStackTrace();
+			log.fatal(e);
+		}
+
+		HibernateUtility.closeSession();
+
+		return categoryList;
 	}
 
 }
